8ddc1980e74fe1f7ef2c8fe0a69e3e1e49a46a47
sumitdey851 <sumitdey851@outlook.com>
2021-07-11 00:13:25 +0530

    Added Loader

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
+// Define constants for HTML elements
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loader
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 
 // Get Quotes From API
 async function getRandomQuote() {
+    // Show loader
+    loading();
     const apiUrl = 'https://api.quotable.io/random';
     try {
         const response = await fetch(apiUrl);
         const apiQuotes = response.json();
+        // Set quote
         apiQuotes.then(function(result) {
 
             // Check quote length to determine styling
@@ -26,6 +43,8 @@ async function getRandomQuote() {
                 authorText.textContent = 'Unknown';
             else
                 authorText.textContent = result.author;
+            // Hide loader
+            complete();
         })
     } catch (error) {
         // handle errors here
