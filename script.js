cf5e46523096395ee26d345afc00778d5436f29c
sumitdey851 <sumitdey851@outlook.com>
2021-07-10 23:21:50 +0530

    Dynamic quote fetch and render

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-// Get Quotes From API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterButton = document.getElementById('twitter');
+const newQuoteButton = document.getElementById('new-quote');
 
-async function getQuotes() {
+
+// Get Quotes From API
+async function getRandomQuote() {
     const apiUrl = 'https://api.quotable.io/random';
     try {
+        const response = await fetch(apiUrl);
+        const apiQuotes = response.json();
+        apiQuotes.then(function(result) {
 
+            // Check quote length to determine styling
+            if (result.content.length > 120)
+                quoteText.classList.add('long-quote');
+            else
+                quoteText.classList.remove('long-quote');
+
+            quoteText.textContent = result.content;
+            
+            // If author is unknown
+            if (!result.author)
+                authorText.textContent = 'Unknown';
+            else
+                authorText.textContent = result.author;
+        })
     } catch (error) {
         // handle errors here
     }
 }
+
+// On Load
+getRandomQuote();
