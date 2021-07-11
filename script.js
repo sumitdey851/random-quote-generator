15031bcb8a95545e230c8ef1404a4b4fa1e1833f
sumitdey851 <sumitdey851@outlook.com>
2021-07-11 09:55:46 +0530

    Code optimization

--- script.js
+++ script.js
@@ -6,14 +6,12 @@ const twitterButton = document.getElementById('twitter');
 const newQuoteButton = document.getElementById('new-quote');
 const loader = document.getElementById('loader');
 
-// Show loader
-function loading() {
+function showLoadingSpinner() {
     loader.hidden = false;
     quoteContainer.hidden = true;
 }
 
-// Hide loader
-function complete() {
+function removeLoadingSpinner() {
     quoteContainer.hidden = false;
     loader.hidden = true;
 }
@@ -21,14 +19,16 @@ function complete() {
 
 // Get Quotes From API
 async function getRandomQuote() {
-    // Show loader
-    loading();
+    showLoadingSpinner();
+    // define a proxyUrl to avoid CORS issue
     const apiUrl = 'https://api.quotable.io/random';
     try {
+        //used in catch block as a terminating condition
+        let maxRequests = 0; 
         const response = await fetch(apiUrl);
-        const apiQuotes = response.json();
-        // Set quote
-        apiQuotes.then(function(result) {
+        const data = response.json();
+        // Set quoteText and authorText
+        data.then(function(result) {
 
             // Check quote length to determine styling
             if (result.content.length > 120)
@@ -43,14 +43,16 @@ async function getRandomQuote() {
                 authorText.textContent = 'Unknown';
             else
                 authorText.textContent = result.author;
-            // Hide loader
-            complete();
+            removeLoadingSpinner();
         })
     } catch (error) {
         // handle errors here
         console.log('Quote fetch failed', error);
         // get quote again
-        getRandomQuote();
+        if (maxRequests > 10)
+            alert('Something is not right. Please try again after sometime.');
+        else
+            getRandomQuote();
     }
 }
 
